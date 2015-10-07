e79382d3d8e8c688022f6de5d0dbae46e636a2e8
Renato Fialho <renato@renatofialho.com>
2015-10-07 07:07:59 +0000

    Generated the colors schemas for the nodes

--- generator.py
+++ generator.py
@@ -9,8 +9,12 @@ interColor = { "Aromatic stacking": "#E36262","Hydrogen bond": "#02B7DB","Hydrop
 cinema = { "H": "#1240AB","K": "#1240AB","R": "#1240AB","D": "#BF3030","E": "#BF3030","N": "#9F3ED5","Q": "#9F3ED5","S": "#9F3ED5", "T": "#9F3ED5",
 "A": "#4EA429","G": "#4EA429","I": "#4EA429","L": "#4EA429","M": "#4EA429","V": "#4EA429","F": "#009999","P": "#009999","W": "#009999","Y": "#009999",
 "C": "#FFAA00",}
-#clustal = { "Aromatic stacking": "#E36262","Hydrogen bond": "#02B7DB","Hydrophobic": "#F0ED67","Repulsive": "#512B8B","Salt bridge": "#519136" }
-#lesk = { "Aromatic stacking": "#E36262","Hydrogen bond": "#02B7DB","Hydrophobic": "#F0ED67","Repulsive": "#512B8B","Salt bridge": "#519136" }
+clustal = { "G": "#FF9640","P": "#FF9640","S": "#FF9640","T": "#FF9640","H": "#BF3030","K": "#BF3030","R": "#BF3030","F": "#1240AB", "W": "#1240AB",
+"Y": "#1240AB","I": "#008500","L": "#008500","M": "#008500","V": "#008500",}
+
+lesk = { "A": "#FF9640","G": "#FF9640","S": "#FF9640","T": "#FF9640","C": "#269926","F": "#269926","I": "#269926","L": "#269926", "M": "#269926",
+"P": "#269926","V": "#269926","W": "#269926","Y": "#269926","H": "#CD0077","N": "#CD0077","Q": "#CD0077","D": "#BF3030","E": "#BF3030","K": "#1240AB",
+"R": "#1240AB",}
 
 def indexinList(List, obj):
     for i in range(len(List)):
@@ -58,18 +62,41 @@ for index, row in csvdf.iterrows():
         obj = {}; obj2 ={}
         obj['name'] = string
         obj['type'] = row['type_atom1']
-        obj['cinema_color'] = cinema.values()[cinema.keys().index(string[0])]
-        #obj['clustal_color'] =
-        #obj['lesk_color'] =
+        try:
+            obj['cinema_color'] = cinema.values()[cinema.keys().index(string[0])]
+        except Exception as e:
+            obj['cinema_color'] = "#ccc"
+
+        try:
+            obj['clustal_color'] = clustal.values()[clustal.keys().index(string[0])]
+        except Exception as e:
+            obj['clustal_color'] = "#ccc"
+
+        try:
+            obj['lesk_color'] = lesk.values()[lesk.keys().index(string[0])]
+        except Exception as e:
+            obj['lesk_color'] = "#ccc"
+
         nodes.append(obj)
-        obj2['cinema_color'] = cinema.values()[cinema.keys().index(string2[0])]
+        try:
+            obj2['cinema_color'] = cinema.values()[cinema.keys().index(string2[0])]
+        except Exception as e:
+            obj2['cinema_color'] = "#ccc"
+        try:
+            obj2['clustal_color'] = clustal.values()[clustal.keys().index(string2[0])]
+        except Exception as e:
+            obj2['clustal_color'] = "#ccc"
+        try:
+            obj2['lesk_color'] = lesk.values()[lesk.keys().index(string2[0])]
+        except Exception as e:
+            obj2['lesk_color'] = "#ccc"
+
         obj2['type'] = row['type_atom2']
         obj2['name'] = string2
         nodes.append(obj2)
 
 # Get only the unique nodes
 nodes =  np.unique(nodes)
-print nodes
 # Get the correct links with the index after the pre-processing
 for index, row in csvdf.iterrows():
     if(row['interaction'] != ""):
