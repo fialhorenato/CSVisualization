76a12ec238fbb108708bfcf318b09502fd7ffcc0
Renato Fialho <renato@renatofialho.com>
2015-10-20 15:33:40 -0200

    Changed the project to get atom sizes

--- generator2.py
+++ generator2.py
@@ -3,6 +3,19 @@ import numpy as np
 import json
 import sys
 
+interColor = { "Aromatic stacking": "#E36262","Hydrogen bond": "#02B7DB","Hydrophobic": "#F0ED67","Repulsive": "#512B8B","Salt bridge": "#519136" }
+
+cinema = { "H": "#1240AB","K": "#1240AB","R": "#1240AB","D": "#BF3030","E": "#BF3030","N": "#9F3ED5","Q": "#9F3ED5","S": "#9F3ED5", "T": "#9F3ED5",
+"A": "#4EA429","G": "#4EA429","I": "#4EA429","L": "#4EA429","M": "#4EA429","V": "#4EA429","F": "#009999","P": "#009999","W": "#009999","Y": "#009999",
+"C": "#FFAA00",}
+
+clustal = { "G": "#FF9640","P": "#FF9640","S": "#FF9640","T": "#FF9640","H": "#BF3030","K": "#BF3030","R": "#BF3030","F": "#1240AB", "W": "#1240AB",
+"Y": "#1240AB","I": "#008500","L": "#008500","M": "#008500","V": "#008500",}
+
+lesk = { "A": "#FF9640","G": "#FF9640","S": "#FF9640","T": "#FF9640","C": "#269926","F": "#269926","I": "#269926","L": "#269926", "M": "#269926",
+"P": "#269926","V": "#269926","W": "#269926","Y": "#269926","H": "#CD0077","N": "#CD0077","Q": "#CD0077","D": "#BF3030","E": "#BF3030","K": "#1240AB",
+"R": "#1240AB",}
+
 # Achar o index de um item em uma lista
 
 def indexinList(list, name):
@@ -11,6 +24,27 @@ def indexinList(list, name):
             index = i
             return index
             break
+
+def findCinemaColor(name):
+    vec = name.split("_")[3];
+    return cinema.values()[cinema.keys().index(vec[0])]
+
+def findLeskColor(name):
+    try:
+        vec = name.split("_")[3];
+        return cinema.values()[lesk.keys().index(vec[0])]
+    except Exception as e:
+        return "#ccc"
+
+def findClustalColor(name):
+    try:
+        vec = name.split("_")[3];
+        return clustal.values()[clustal.keys().index(vec[0])]
+    except Exception as e:
+        return "#ccc"
+
+def findlinkcolor(name):
+    return interColor.values()[interColor.keys().index(name.strip())]
 
 # Get the parameters
 params = sys.argv
@@ -30,8 +64,14 @@ for index, row in csvdf.iterrows():
     if(row['interaction'] != ""):
         if(row['atom1'] != row['atom2']):
             obj['name'] = row['atom1']
+            obj['cinema'] = findCinemaColor(obj['name'])
+            obj['clustal'] = findClustalColor(obj['name'])
+            obj['lesk'] = findLeskColor(obj['name'])
             nodes.append(obj)
             obj2['name'] = row['atom2']
+            obj2['cinema'] = findCinemaColor(obj2['name'])
+            obj2['clustal'] = findClustalColor(obj2['name'])
+            obj2['lesk'] = findLeskColor(obj2['name'])
             nodes.append(obj2)
             groups.append(row['atom1'].split("_")[3] + row['atom1'].split("_")[4])
             groups.append(row['atom2'].split("_")[3] + row['atom2'].split("_")[4])
@@ -56,6 +96,8 @@ for index, row in csvdf.iterrows():
         if(row['atom1'] != row['atom2']):
             link['source'] = indexinList(nodes,row['atom1'])
             link['target'] = indexinList(nodes,row['atom2'])
+            link['color'] = findlinkcolor(row['interaction'])
+            link["type"] = row['interaction']
             links.append(link)
 
 
